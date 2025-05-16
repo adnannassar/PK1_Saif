80d67848e3d936cd6b2bedc2632d6a82fff42db4
saifiq <Saif.Al.Suleyman@hhu.de>
2025-05-16 13:26:42 +0200

    Code Format

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,17 +2,18 @@ package src;
 
 public class Calculator {
     // TODO ADNAN add
-    public static int add(int a, int b){
-        return a+b;
+    public static int add(int a, int b) {
+        return a + b;
     }
 
     // TODO SAIF sub
-    public static int sub(int a, int b){
-        return a-b;
+    public static int sub(int a, int b) {
+        return a - b;
     }
-// TODO SAIF mult
-    public static int Mult(int a , int b){
-        return a * b ;
+
+    // TODO SAIF mult
+    public static int Mult(int a, int b) {
+        return a * b;
     }
 }
 
